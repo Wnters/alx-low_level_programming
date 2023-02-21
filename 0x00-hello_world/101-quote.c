e55aef43519fbfd4f58e7d0af6f3d250f27f25c7
Aaron Chiraerae <achiraerae@gmail.com>
2023-02-20 22:51:25 -0800

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	chac* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fputs(str, stdout);
 	return (1);
 }
